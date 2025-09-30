a27719f7dfb5a4a93e769f296db443a0b7fef453
Phillip Langland <philliplangland@gmail.com>
2025-09-30 15:59:23 -0500

    forgot to update hex-docs.js

--- hex-docs.js
+++ hex-docs.js
@@ -1,5 +1,9 @@
 var versionNodes = [
   {
+    version: "v0.2.1", // version number or name (required)
+    url: "https://hexdocs.pm/white_rabbit/0.2.1/" // documentation URL (required)
+  },
+  {
     version: "v0.2.0", // version number or name (required)
     url: "https://hexdocs.pm/white_rabbit/0.2.0/" // documentation URL (required)
   }
